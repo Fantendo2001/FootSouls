7c6ef95ac057a30cf002f391e72b84170204a2f1
Fantendo2001 <119010462@link.cuhk.edu.cn>
2022-06-29 22:03:55 +0800

    FIx master

--- FootSouls_UE5/Source/FootSouls/cppTest.cpp
+++ FootSouls_UE5/Source/FootSouls/cppTest.cpp
@@ -2,8 +2,7 @@
 
 
 #include "cppTest.h"
-const APlayerController* PlayerController;
-const FVector ForwardVector;
+
 // Sets default values
 AcppTest::AcppTest()
 {
@@ -21,18 +20,4 @@ void AcppTest::BeginPlay()
 void AcppTest::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	// print log to unreal console
-	GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, FString::Printf(TEXT("Hello World")));
-	//////////////////////////////////////////////////////////////////////////
-	// get the first person camera
-	const APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
-	// Get the direction of the camera
-	const FVector ForwardVector = PlayerController->GetControlRotation().Vector();
-	// log forward vector in unreal console
-	UE_LOG(LogTemp, Warning, TEXT("ForwardVector: %s"), *ForwardVector.ToString());
-	// get the location of the camera
-	const FVector Location = PlayerController->GetPawn()->GetActorLocation();
-	// log location in unreal console
-	UE_LOG(LogTemp, Warning, TEXT("Location: %s"), *Location.ToString());
-	//////////////////////////////////////////////////////////////////////////
 }
